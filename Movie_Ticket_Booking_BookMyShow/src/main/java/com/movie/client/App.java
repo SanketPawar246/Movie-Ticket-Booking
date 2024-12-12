2e9f634030d428647da01905f3497f58ad1fc214
Sanket Prabhu Pawar <Sanket Prabhu Pawar@LAPTOP-TOVV455I>
2024-12-12 14:29:57 +0530

    asddsf

--- Movie_Ticket_Booking_BookMyShow/src/main/java/com/movie/client/App.java
+++
@@ -1,8 +0,0 @@
-package com.movie.client;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("57854785448");
-    }
-}
